aec5a2fbc4842d79560c59de75487b824160cae0
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-29 23:38:48 +0530

    code

--- router/routes.go
+++ router/routes.go
@@ -3,16 +3,18 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/robert/notification/handler"
+	"github.com/robert/notification/middlewares"
 )
 
 func Startserver() {
 
 	r := gin.Default()
 	r.Static("/assets", "./assets")
-	r.LoadHTMLGlob("/templates/*.html")
+	r.LoadHTMLGlob("templates/*.html")
+	r.Use(middlewares.ErrorHandler) // centralized error handling
 
 	// endpoints -------------
-	r.GET("/", handler.Homepage)
+	r.GET("/", middlewares.Isloggedin(), handler.Homepage)
 	r.GET("/login", handler.Loginget)
 	r.POST("/login", handler.Loginpost)
 	r.GET("/secret", handler.Secretpage)
