ce336965985fcfa4eb58011b8ad27eafeb1ce1a7
robertcjoshy <robertcjoshy11@gmail.com>
2024-04-29 23:38:15 +0530

    code

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,19 +1,30 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/robert/notification/middlewares"
 	"github.com/robert/notification/model"
 )
 
 func Homepage(c *gin.Context) {
-
-	c.HTML(http.StatusOK, "home.html", gin.H{})
+	username, _ := c.Get("username")
+	fmt.Println("user = ", username)
+	c.Abort()
+	fmt.Println("after abort")
+	c.HTML(http.StatusOK, "home.html", gin.H{"username": username})
 }
 
 func Loginget(c *gin.Context) {
-	c.HTML(http.StatusOK, "secret.html", gin.H{})
+	result := middlewares.GetJWT(c)
+	fmt.Println("gettoken = ", result)
+	if !result {
+		c.HTML(http.StatusOK, "login.html", gin.H{})
+	} else {
+		c.Redirect(http.StatusFound, "/secret")
+	}
 }
 
 func Loginpost(c *gin.Context) {
@@ -21,13 +32,22 @@ func Loginpost(c *gin.Context) {
 	username := c.PostForm("username")
 	password := c.PostForm("password")
 
-	if _, err := model.Getpassword(username, c); err == nil {
-		c.Redirect(http.StatusFound, "/secret")
+	pass, err := model.Getpassword(username, c)
+
+	if err == nil && pass == password {
+		token := middlewares.CreateJWT(1, username, c)
+		fmt.Println(token)
+		//c.Header("Authorization", "Bearer "+token) // sending jwt token along with header
+		c.SetCookie("jwt_token", token, 3600, "/", "", false, true)
+		fmt.Println("sending header")
+		//c.Redirect(http.StatusFound, "/secret")
+		c.HTML(http.StatusOK, "secret.html", gin.H{})
+
 	} else {
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"user": username,
-			"pass": password,
-		})
+		errr := middlewares.NewAppError(http.StatusUnauthorized, "invalid credentials")
+		//c.Error(errors.New("testing error"))
+		c.Error(errr)
+		return
 	}
 }
 
@@ -37,7 +57,8 @@ func Secretpage(c *gin.Context) {
 }
 
 func Logout(c *gin.Context) {
-
+	//result := middlewares.GetJWT(c)
+	c.SetCookie("jwt_token", "", -1, "/", "", false, true)
 	c.HTML(http.StatusOK, "home.html", gin.H{})
 }
 
@@ -50,7 +71,9 @@ func Createuser(c *gin.Context) {
 
 	// Bind form data to the user struct
 	if err := c.Bind(&input); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "cannot bind json"})
+		errr := middlewares.NewAppError(http.StatusBadRequest, "cannot binf json")
+		//c.JSON(http.StatusBadRequest, gin.H{"error": "cannot bind json"})
+		c.Error(errr)
 		return
 	}
 	// save to database
